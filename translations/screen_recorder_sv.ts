584ac7d43302bf145ad73e11695b9a3a9807716a
Åke Engelbrektson <eson57@users.noreply.github.com>
2023-08-16 13:19:19 +0200

    Create screen_recorder_sv.ts
    Add Swedish translation

---
+++ translations/screen_recorder_sv.ts
@@ -0,0 +1,243 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="40"/>
+        <source>About Application</source>
+        <translation>Om appen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="50"/>
+        <source>#descriptionText</source>
+        <translation>&lt;p&gt;Skärminspelningsapp&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="55"/>
+        <source>GNU General Public License, version 2</source>
+        <translation>GNU General Public License, version 2</translation>
+    </message>
+    <message>
+        <source>3-Clause BSD License</source>
+        <translation type="vanished">3-Clause&#xa0;BSD License</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="65"/>
+        <source>#licenseText</source>
+        <translation>&lt;p&gt;&lt;i&gt;Copyright (C) 2023 Yura Beznos at You-ra.info&lt;/i&gt;&lt;/p&gt;
+&lt;p&gt;&lt;i&gt; This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.&lt;/i&gt;&lt;/p&gt;
+&lt;p&gt;&lt;i&gt; This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.&lt;/i&gt;&lt;/p&gt;
+&lt;p&gt;&lt;i&gt; You should have received a copy of the GNU General Public License
+ along with this program; if not, write an email to the license@you-ra.info&lt;/i&gt;&lt;/p&gt;
+        </translation>
+    </message>
+</context>
+<context>
+    <name>DefaultCoverPage</name>
+    <message>
+        <source>Screen recorder</source>
+        <translation type="vanished">Screen recorder</translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/DefaultCoverPage.qml" line="49"/>
+        <source>Screen Recorder</source>
+        <translation>Skärminspelare</translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/DefaultCoverPage.qml" line="58"/>
+        <source>stopped</source>
+        <translation>stoppad</translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/DefaultCoverPage.qml" line="60"/>
+        <source>recording...</source>
+        <translation>spelar in...</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="77"/>
+        <source>Your recordings will be shown here</source>
+        <translation>Dina inspelningar kommer att visas här</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="47"/>
+        <source>Screen recorder</source>
+        <translation>Skärminspelare</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="48"/>
+        <source>Record button - to record your screen. Settings in pulley menu to change properties.</source>
+        <translation>Inspelningsknapp - För att spela in din skärm. Inställningar i toppmenyn, för att ändra egenskaper.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="107"/>
+        <source>Warning! You may need to install lipstick2vnc package to be able to record screen.</source>
+        <translation>Varning! Du kan behöva installera paketet lipstick2vnc, för att kunna spela in skärmen.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="119"/>
+        <source>Record</source>
+        <translation>Spela in</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="121"/>
+        <source>Stop</source>
+        <translation>Stopp</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="70"/>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+</context>
+<context>
+    <name>RecordingDelegate</name>
+    <message>
+        <location filename="../qml/components/RecordingDelegate.qml" line="9"/>
+        <source>Share</source>
+        <translation>Dela</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/RecordingDelegate.qml" line="22"/>
+        <source>Rename</source>
+        <translation>Byt namn</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/RecordingDelegate.qml" line="30"/>
+        <source>Delete</source>
+        <translation>Ta bort</translation>
+    </message>
+    <message>
+        <location filename="../qml/components/RecordingDelegate.qml" line="32"/>
+        <source>Deleting</source>
+        <translation>Tar bort</translation>
+    </message>
+</context>
+<context>
+    <name>RecordingsModel</name>
+    <message>
+        <location filename="../src/recordingsmodel.cpp" line="165"/>
+        <source>Today</source>
+        <translation>Idag</translation>
+    </message>
+    <message>
+        <location filename="../src/recordingsmodel.cpp" line="169"/>
+        <source>Yesterday</source>
+        <translation>Igår</translation>
+    </message>
+    <message>
+        <location filename="../src/recordingsmodel.cpp" line="173"/>
+        <source>This week</source>
+        <translation>Denna vecka</translation>
+    </message>
+    <message>
+        <location filename="../src/recordingsmodel.cpp" line="177"/>
+        <source>This month</source>
+        <translation>Denna månad</translation>
+    </message>
+    <message>
+        <location filename="../src/recordingsmodel.cpp" line="181"/>
+        <source>Last 6 months</source>
+        <translation>Senaste 6 månaderna</translation>
+    </message>
+    <message>
+        <location filename="../src/recordingsmodel.cpp" line="183"/>
+        <source>Older</source>
+        <translation>Äldre</translation>
+    </message>
+</context>
+<context>
+    <name>RenameDialog</name>
+    <message>
+        <location filename="../qml/pages/RenameDialog.qml" line="22"/>
+        <source>Rename</source>
+        <translation>Byt namn</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RenameDialog.qml" line="28"/>
+        <source>New filename</source>
+        <translation>Nytt filnamn</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RenameDialog.qml" line="42"/>
+        <source>A file name must be specified</source>
+        <translation>Ett filnamn måste anges</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RenameDialog.qml" line="44"/>
+        <source>File already exists</source>
+        <translation>Filen finns redan</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="41"/>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="45"/>
+        <source>Scale</source>
+        <translation>Skala</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="53"/>
+        <source>%1:%2</source>
+        <translation>%1:%2</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="54"/>
+        <source>Scale of video</source>
+        <translation>Videoskala</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="62"/>
+        <source>Frame per second</source>
+        <translation>Bildrutor per sekund</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="70"/>
+        <source>%1fps</source>
+        <translation>%1fps</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="71"/>
+        <source>fps</source>
+        <translation>fps</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="79"/>
+        <source>Bitrate</source>
+        <translation>Bithastighet</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="87"/>
+        <source>%1K bps</source>
+        <translation>%1K bps</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/Settings.qml" line="88"/>
+        <source>bps</source>
+        <translation>bps</translation>
+    </message>
+</context>
+<context>
+    <name>ViewVideoPage</name>
+    <message>
+        <location filename="../qml/pages/ViewVideoPage.qml" line="107"/>
+        <source>Failed to play</source>
+        <extracomment>Full page placeholder shown when a video failed to load</extracomment>
+        <translation>Kunde inte spela upp</translation>
+    </message>
+</context>
+</TS>
